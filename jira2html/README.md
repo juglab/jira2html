b4f2fd31275252ec5c5a305b14306cb0a4ff774c
Gabriella Turek <turek@mpi-cbg.de>
2019-09-16 10:50:49 +0200

    Initial commit to git. Script tested and documented

---
+++ jira2html/README.md
@@ -0,0 +1,34 @@
+# jira2html
+Automatically output a list of JIRA issues to an HTML table and push them to a git-hub repository. The original target for this exercise was a page to be published to a Wiki.js wiki backed by git.
+
+## Required packages:
+
+* [PyGithub](https://github.com/PyGithub/PyGithub): pip install PyGithub
+* [JIRA Python](https://jira.readthedocs.io/en/latest/installation.html): pip install jira
+* [configobj](https://configobj.readthedocs.io/en/latest/configobj.html): pip install configobj
+
+## How to:
+
+* You need have to create a github access token [here](https://github.com/settings/tokens) (Select only 'repo' as scope).
+* You need to provide your JIRA authentication parameters either as the environment variables (JIRA\_USR and JIRA\_PWD]  or as arguments to the script. 
+* You need to create a configuration files using the provided template. The script will automatically look for a local configuration file named "jira2html.conf", 
+  but you can also use the "-c" command line option to specify an alternative configuration file
+
+The configuration file must contain:
+
+```
+# Template config file for jira2md.py
+jira_url=                           # URL of your JIRA server
+jira_project=                       # Your JIRA project code
+git_token=                          # Obtained from https://github.com/settings/tokens
+git_repo=                           # yourOrg/yourRepo
+commit_message=                     # Git commit message
+status_filter=True                  # If true shows only open and in progress issues, set to false for all issues
+md_file=                            # git repo path of output file
+```
+
+## Usage
+
+```
+>> jira2html.py [-h] [-u <user>] [-p <password>] [-c <configFile>]
+```
---
+++ jira2html/jira2html.py
@@ -0,0 +1,132 @@
+import argparse
+import os
+import datetime
+import sys
+from jira import JIRA
+from configobj import ConfigObj
+from github import Github
+
+def main(argv):
+     
+    today = datetime.date(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day)
+    
+    # Configuration file (required, assume it's in the same dir as script, option to specify a new config available)
+    config_file = 'jira2html.conf'
+    
+    # Set up argument parser (none required)
+    parser = argparse.ArgumentParser(description='Process input arguments.')
+    parser.add_argument('-u', metavar='<user>', dest='usr', help='JIRA user name. Required if not set via the env. variable JIRA_USR.')
+    parser.add_argument('-p', metavar='<password>', dest = 'pwd', help='JIRA user password. Required if not set via the env. variable JIRA_PWD.')
+    parser.add_argument('-c', metavar='<configFile>', dest='configFile', help='Absolute path to config file. If not present, will look for ./jira2md.conf')
+
+    # Parse arguments
+    args = parser.parse_args()
+    
+    if (args.usr == None and args.pwd == None):
+        try: 
+            usr = os.environ['JIRA_USR']
+            pwd = os.environ['JIRA_PWD']
+        except:
+            print("Error: server authentication information (user and/or password) is missing.\n")
+            parser.print_help()
+            sys.exit(1)
+    else:
+        usr = args.usr
+        pwd = args.pwd
+        
+        
+    # Read config file
+    try:
+        config = ConfigObj(config_file)
+    except:
+        print("Error: config file not found or unreadable.\n")
+        
+    jira_url = config['jira_url']   
+    try:    
+        jira = JIRA(jira_url, auth=(usr, pwd))
+    except:
+        print("Error: JIRA server authentication failed.\n")
+        sys.exit(1)
+    
+    issues = jira.search_issues('project="' + config['jira_project'] + '"',maxResults=0)
+    issues.sort(key=lambda x: x.key, reverse=True)
+    
+    issues_table = create_issues_table(jira_url, issues, config['status_filter'])
+    
+    # Connect to git repo
+    try:
+        g = Github(config['git_token'])
+    except:
+        print("Error: git authentication failed.\n")
+        sys.exit(1)
+        
+    repo = g.get_repo(config['git_repo'])
+    
+    # Push changes to git
+    try:
+        content = repo.get_contents(config['md_file'])
+    except:
+        print("Warning: target not found, a new file will be created.\n")
+
+    if content == None:
+        new_content = create_header(config['commit_message'], 'Auto-generated issues list from JIRA') + '\n' + issues_table
+        repo.create_file(config['md_file'], config['commit_message'] + " " + str(today), new_content, branch="master")
+    else:
+        tmp = content.decoded_content.decode("utf-8").split("\n")
+        # Remove current header
+        current_text = ""
+        for i in range(7, len(tmp)):
+            current_text += tmp[i] + "\n"
+        # Remove existing table
+        edited_current = current_text[:current_text.rfind('<table')]
+        new_content = create_header(config['commit_message'], "Auto generated issues list from JIRA") + "\n" + edited_current + issues_table
+        repo.update_file(content.path, config['commit_message'] + " " + str(today), new_content, content.sha, branch="master")
+        
+    print("Update pushed, all done!")
+        
+                
+def create_issues_table(jira_url, issues, status_filter):
+    
+    status = {} 
+    if (status_filter == 'True'):
+        status = {'Open':'#E0E0E0', 'In Progress':'#99FFF', 'Selected':'#E0E0E0', 'To Do':'#E0E0E0'} 
+        
+    text = \
+        '<table border="0" cellpadding="0" cellspacing="1">\n' + \
+        '<theader>\n' + \
+        '    <tr align="center" style="background-color: #60a9a9;" valign="middle">\n' + \
+        '        <td>ISSUE</td>\n' + \
+        '        <td>SUMMARY</td>\n' + \
+        '        <td>PRIORITY</td>\n' + \
+        '        <td>STATUS</td>\n' + \
+        '    </tr>\n' + \
+        '</theader>\n' + \
+        '<tbody>\n'
+        
+    for issue in issues:
+        if (len(status) != 0 and issue.fields.status.name not in status.keys()):
+                continue
+        text += \
+        '    <tr>\n' + \
+        '        <td><a href="' + jira_url  + '/browse/' +  issue.key + '">' + issue.key + '</a></td>\n' + \
+        '        <td>' + issue.fields.summary + '</td>\n' + \
+        '        <td>' + issue.fields.priority.name + '</td>\n' + \
+        '        <td style="background-color:' + status[issue.fields.status.name] + '">' + issue.fields.status.name + '</td>\n' + \
+        '    </tr>\n'
+        
+    text +=    '</tbody>\n' + \
+        '</table>\n'
+    return text
+
+                
+def create_header(title, description, published='true'):
+    return '---\n' + \
+             'title: ' + title + '\n'+ \
+             'description: ' + description + '\n'+ \
+             'published: ' + published + '\n'+ \
+             'date: ' + str(datetime.datetime.today()) + '\n'+ \
+             'tags: \n' + \
+             '---'
+             
+if __name__ == '__main__':
+    main(sys.argv[1:])
